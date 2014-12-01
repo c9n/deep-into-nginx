d70f981bdbdbcd45a0c3a82e9b3d8d017bafc7e5
Lynn <homlean@gmail.com>
2014-11-30 21:25:21 -0500

    from Mac to CentOS
    force cleanup

--- misc/build.sh
+++ misc/build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 #
 # USR: Lynn
-# OS : OS X Yosemite
+# OS : CentOS 6.5
+# RAM: 1GB
 #
 
 cd ../nginx
 
-./configure --with-debug --with-http_ssl_module --with-cc-opt="-Wno-deprecated-declarations -g -O0"
+./configure --with-debug --with-http_ssl_module --with-cc-opt="-g -O0"
 
 make -B CFLAGS="-g -O0"
+
+rm -rf /usr/local/nginx
 
 make install
 

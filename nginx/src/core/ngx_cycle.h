c82230f5a535f09f33cf7af4d72879c0f13efc14
Lynn <homlean@gmail.com>
2015-11-05 21:05:28 +0800

    four asterisks
    ✨✨✨✨

--- nginx/src/core/ngx_cycle.h
+++ nginx/src/core/ngx_cycle.h
@@ -35,7 +35,7 @@ struct ngx_shm_zone_s {
 
 
 struct ngx_cycle_s {
-    void                  ****conf_ctx;
+    void                  ****conf_ctx; // oops !!!
     ngx_pool_t               *pool;
 
     ngx_log_t                *log;

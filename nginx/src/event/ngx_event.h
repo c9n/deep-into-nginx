1cfa01cf0322f15266c8f3b1eeda7f18360bc550
Lynn <homlean@gmail.com>
2015-11-12 14:16:59 +0800

    meet ngx events
    When I was young, I observed that nine out of ten things I did were
    failures, so I did ten times more work.

--- nginx/src/event/ngx_event.h
+++ nginx/src/event/ngx_event.h
@@ -216,7 +216,7 @@ struct ngx_event_aio_s {
 
 #endif
 
-
+/* Oops */
 typedef struct {
     ngx_int_t  (*add)(ngx_event_t *ev, ngx_int_t event, ngx_uint_t flags);
     ngx_int_t  (*del)(ngx_event_t *ev, ngx_int_t event, ngx_uint_t flags);
